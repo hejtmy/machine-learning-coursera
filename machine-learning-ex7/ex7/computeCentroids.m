639776d79d0057f4a468abdc35846876fc1d45c7
hejtmy <hejtmy@gmail.com>
2017-11-23 15:05:56 -0800

    Solution to the second problem week 7

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -26,7 +26,10 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
-
+for iC = 1:K
+  values = X(idx == iC, :);
+  centroids(iC, :) = mean(values, 1);
+endfor
 
 
 
